6353e832d03bb46b31582c2314fbf46961ad6d6b
chrisnastro <cnastro@icloud.com>
2024-09-14 12:40:05 -0400

    fixed mobile dropdown color

--- forms/contact.php
+++ forms/contact.php
@@ -40,11 +40,3 @@
   echo $contact->send();
 ?>
 
-<?php
-$name =$_POST['name'];
-$from =$_POST['email'];
-$message =$_POST['message'];
-$to="info@webbiskit.com";
-$headers= "MIME-VERSION: 1.0" . "\r\n";
-$headers .="Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers ="From : <$from> \r\n ";
